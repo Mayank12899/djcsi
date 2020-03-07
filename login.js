56d166ebc091845127de4bfbc8f1627e6e93d83b
Mehek Male <mehekmaley@gmail.com>
2020-03-08 02:44:18 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,43 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+var login = document.getElementById("login");
+login.addEventListener('click',signin);
+
+function signin() {
+    console.log("hello")
+    var userEmail = document.getElementById("nname").value;
+    var userPass = document.getElementById("fnumb").value;
+  
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    })
+}
+var logout = document.getElementById('logout');
+    logout.addEventListener('click',logo)
+    function logo() {
+        
+        firebase.auth().signOut();
+        console.log("login.html");
+      
+    }
+firebase.auth().onAuthStateChanged(function(user) {
+    if(user) {
+     
+        window.location.href = 'home.html';
+        console.log("good")
+    
+      
+    }
+    else{
+      console.log("bad")
+     
+    }
+    });
+    
+    
+    
