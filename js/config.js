9bdcd0fb615545c4b1bee8b1d2b00263a040331a
mehekmaley <mehekmaley@gmail.com>
2020-03-07 21:47:57 +0530

    f

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDrmNE1jwPmCQ0vjXb9F8H7n2iwQe77ReE",
+    authDomain: "depression-bot-sujmkv.firebaseapp.com",
+    databaseURL: "https://depression-bot-sujmkv.firebaseio.com",
+    projectId: "depression-bot-sujmkv",
+    storageBucket: "depression-bot-sujmkv.appspot.com",
+    messagingSenderId: "101338960061",
+    appId: "1:101338960061:web:6f86ae2b9fe3557c17a7d4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
